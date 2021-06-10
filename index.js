0c9c513bf9f6b116af0a50fe11cdfa2d99b8d402
User GCT <yuri.ouverney@hotmai.com>
2021-06-10 10:32:04 -0300

    importando bootstrap

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 //setando o express para utilizar o EJS como view engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index')
